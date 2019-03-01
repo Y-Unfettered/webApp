98afd96edb2e4d052fbd01e6a9e66ef95c4f6413
xiaoking <934064468@qq.com>
2019-03-01 14:29:09 +0800

    header实现

--- webApp/src/App.vue
+++ webApp/src/App.vue
@@ -1,23 +1,48 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- header -->
+    <app-header :poiInfo="poiInfo"></app-header>
+    <!-- navigation -->
+    <app-nav></app-nav>
+    <!-- content -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Header from './components/header/Header'
+import Nav from '@/components/nav/Nav'
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return{
+      poiInfo:{}
+    }
+  },
+  components: {
+    "app-header":Header,
+    "app-nav":Nav
+  },
+  created(){
+    // axios
+
+    // fetch
+    fetch("/api/goods")
+      .then(res => {
+        return res.json()
+      })
+      .then(response =>{
+        // console.log(response)
+        if(response.code == 0){
+          // console.log(response.data)
+          this.poiInfo = response.data.poi_info
+          // console.log(this.poiInfo)
+        }
+      })
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
